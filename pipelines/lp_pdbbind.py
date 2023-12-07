c4c82e0af014c2e2106bc7d3e7728c42e0d67759
mwisniewskidrugdesign <152893708+mwisniewskidrugdesign@users.noreply.github.com>
2023-12-07 12:28:31 +0100

    Update lp_pdbbind.py
    create lp_pdbbind pipeline

--- pipelines/lp_pdbbind.py
+++ pipelines/lp_pdbbind.py
@@ -1,1 +1,18 @@
+from protein_ligand import generator
+from protein_ligand import docking
+from protein_ligand import datasets
+import pandas as pd
 
+pd.set_option('display.max_columns', None)
+
+generate_library_step = True
+convert_step=True                #ADD IF !!!!!!!!!!!!!!!!!!!!!!!!!! SUCH US DOCKING PROGRAMS LIST
+docking_step=True
+
+def pipeline(datadir,lp_pdbbind_datadir,lp_pdbbind_df,no_mode):
+  if generate_library_step:
+    '''Generate the library for LP_PDBBIND operations'''
+    generator.generate_libraray(datadir)
+    workspace = generator.GetDataset(datadir,lp_pdbbind_df)                                    #create workspace
+    #workspace.bdb2020plus(lp_pdbbind_dir)                                                      #copy files to workspace
+      
