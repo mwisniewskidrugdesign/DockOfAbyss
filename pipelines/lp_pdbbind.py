e53ac9796c73fa6c311b0aa6f02d1cdda67e2da6
maciejwisniewskidrugdesign <m.wisniewski@datascience.edu.pl>
2023-12-08 00:27:04 +0100

    diagonal_pipeline for lpdbind

--- pipelines/lp_pdbbind.py
+++ pipelines/lp_pdbbind.py
@@ -10,34 +10,63 @@ convert_step=True                #ADD IF !!!!!!!!!!!!!!!!!!!!!!!!!! SUCH US DOCK
 docking_step=True
 docking_programs=['smina']
 
-def pipeline(datadir,lp_pdbbind_datadir,lp_pdbbind_df,no_mode):
+def diagonal_pipeline(datadir,rawdir,df,no_mode):
   #prep DF step for Clear 1 or Clear 2 !!!!
   if generate_library_step:
     '''Generate the library for LP_PDBBIND operations'''
     generator.generate_libraray(datadir)
-    workspace = generator.GetDataset(datadir,lp_pdbbind_datadir,lp_pdbbind_df)                                    #create workspace
+    workspace = generator.GetDataset(datadir,rawdir,df)                                    #create workspace
     workspace.lp_pdbbind()                                                    #copy files to workspace
 
   if convert_step:
-    for index,row in lp_pdbbind_df.iterrows():
+    for index,row in df.iterrows():
       print(str(index) + '.' + row['pdbid'])
       library = generator.Converter(row['pdbid'],datadir)
       library.pdb_to_pdbqt(protein=True,pocket=True,ligand=True,native_ligand=True)
       ### add for other
 
   if docking_step:
-    df_prep = datasets.DatasetPreparation(lp_pdbbind_df)
+    df_prep = datasets.DatasetPreparation(df)
     molecules = df_prep.get_molecules('pdbid')
 
-
-
-    ##### TO DO
     if 'smina' in docking_programs:
-      smina_docking = docking.Smina(datadir)
-      smina_docking.smina_dirs()
-          ## ????
-    if 'smina' in docking_programs:
-      smina_docking_error_number = 0
-      while True:
-        try:
+      smina_docking = docking.Smina(datadir)  # SMINA Docking Class
+      smina_docking.smina_dirs()  ## Generate output dirs for SMINA docking
+      smina_matrix = smina_docking.create_smina_matrix(molecules[:], molecules[:], no_modes)
 
+    # if 'rxdock' in docking_programs:
+    #     rx_docking = docking.RxDock(datadir)
+    #     rx_docking.rxdock_dirs()
+    #     rxdock_matrix = None
+
+    for molecule_idx, molecule in enumerate(molecules[:]):  ## Docking Loop for molecules from list generated earlier
+      print('Docking ' + molecule + ' to ' + molecule + '. With: \n', docking_programs)  ## Print PDB structure code
+      if 'smina' in docking_programs:
+        smina_docking_error_number = 0
+        while True:  ## Loop - necessery to generate exactly 100 modes, sometimes with random seed it's generating smaller number of conforms
+          try:
+            smina_docking.smina_files(molecule, molecule, molecule)  ## set variables for specific molecule files
+            smina_docking.smina_docking(no_modes)  ## smina docking function
+            smina_docking.read_experimental_affinity(df, molecule, molecule)  ## reading experimental affinity data for specific molecule from dataframe
+            smina_docking.read_scoring_function()  ## reading scoring function predicted binding affinity from output
+            smina_docking.read_atom_term_function(no_modes)  ## reading atom terms sf's components from output
+            smina_docking.fill_smina_matrix(molecule_idx, molecule_idx)  ## fill smina matrix with output datas                      ### MAYBE inserts read_* functions into this one
+          except:
+            smina_docking_error_number += 1
+            print('Smina proposed less modes than expected for docking ' + molecule + ' to ' + molecule + '. ' + str(smina_docking_error_number) + 'st time.')
+            continue
+          break
+
+      # if 'rxdock' in docking_programs:
+      #     rx_docking_error_number = 0
+      #     rx_docking.rxdock_files(molecule,molecule,molecule)
+      #     rx_docking.rxdock_system_preparation()
+      #     #while True:
+      #         #try:
+      #             #print(elo)
+      #         #except:
+      #             #rx_docking_error_number+=1
+      #             #print('RxDock proposed less modes than expected for docking ' + molecule + ' to ' + molecule + '. ' + rx_docking_error_number + 'st time.')
+
+    if 'smina' in docking_programs:
+            smina_docking.save_matrix('smina_matrix')                                       ## save SMINA matrix
