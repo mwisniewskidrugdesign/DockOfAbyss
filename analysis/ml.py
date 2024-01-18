f775ee391cd7e8a067a21a420ccba4d17732fbec
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-01-18 10:03:34 +0100

    smina matrix fix

---
+++ analysis/ml.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import sklearn
+
+
+
+class Diagonal:
+    def __init__(self,matrix,datadir):
+        return None
+
+def extract_diagonals(matrix):
+    return None
