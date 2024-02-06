0f3339351c3c2f898479577660457146d6dab9a5
mwisniewskidrugdesign <m.wisniewski@datascience.edu.pl>
2024-02-06 18:08:26 +0100

    adding dictionaries - tbc

--- protein_ligand/generator.py
+++ protein_ligand/generator.py
@@ -52,10 +52,10 @@ def generate_libraray(datadir):
 
 class GetDataset:
     def __init__(self,datadir,rawdir,df,pdb_id_column):
-        self.datadir = datadir              #directory of data
-        self.rawdir = rawdir                #directory of rawdata
-        self.df = df                        #dataframe of dataset
-        self.pdb_id_column=pdb_id_column    #pdb_id_columnn = 'pdbid' or diffrent
+        self.datadir = datadir              #  data directory to our workspace
+        self.rawdir = rawdir                #  directory to our raw data (specific for chosen dataset), if you are gonna use your own, type None, and skip convert step
+        self.df = df                        #  path to the dataframe with yours complexes
+        self.pdb_id_column=pdb_id_column    #  data frame column with specified pdb_id column
     def bdb2020plus(self,native_ligand=False):
         '''
         Copy files from BindindDB 2020+ dataset
@@ -96,16 +96,22 @@ class GetDataset:
         except IndexError:
             print('Error!')
     def lp_pdbbind(self,native_ligand=False):
+        '''
+        Copy files from Leak Proof - Protein Data Bank Bind dataset
+        lp_pdbbnd = directory of LPPDBBind database
+        native_ligand = False (deafult), if you want native ligand be diffrent than docked oen -> True
+        '''
         try:
+
+            pdb_protein_final_path = self.datadir + '/protein/pdb'
+            pdb_pocket_final_path = self.datadir + '/pocket/pdb'
+            mol2_native_ligand_final_path = self.datadir + '/native_ligand/mol2'
+            sdf_native_ligand_final_path = self.datadir + '/native_ligand/sdf'
+            mol2_ligand_final_path = self.datadir + '/ligand/mol2'
+            sdf_ligand_final_path = self.datadir + '/ligand/sdf'
+
             for index,row in self.df.iterrows():
                 print(str(index) + '.' + row[self.pdb_id_column])
-
-                pdb_protein_final_path = self.datadir + '/protein/pdb'
-                pdb_pocket_final_path = self.datadir + '/pocket/pdb'
-                mol2_native_ligand_final_path = self.datadir + '/native_ligand/mol2'
-                sdf_native_ligand_final_path = self.datadir + '/native_ligand/sdf'
-                mol2_ligand_final_path = self.datadir + '/ligand/mol2'
-                sdf_ligand_final_path = self.datadir + '/ligand/sdf'
 
                 protein_pdb_file = self.rawdir +'/'+row[self.pdb_id_column]+'/'+row[self.pdb_id_column]+'_protein.pdb'
                 pocket_pdb_file = self.rawdir +'/'+row[self.pdb_id_column]+'/'+row[self.pdb_id_column]+'_pocket.pdb'
@@ -130,116 +136,170 @@ class GetDataset:
                     if not os.path.exists(final_paths[i]+'/'+row[self.pdb_id_column]+strings[i]):
                         shutil.copy(files[i],final_paths[i]+'/'+row[self.pdb_id_column]+strings[i])
         except IndexError:
-            print('Error!')
+            print('There is no directory!')
 class Converter:
     def __init__(self,datadir,molecule):
-        self.molecule = molecule
-        self.datadir = datadir
+        self.molecule = molecule    #  specify molecule PDB ID from your dataframe
+        self.datadir = datadir      #  data directory to our workspace
+        self.converting_dictionary = {molecule:{}}
         settings.init()
-    def pdb_to_pdbqt(self,protein=False,pocket=False,ligand=False,native_ligand=False):
+    def pdb_to_pdbqt(self,
+                     protein=False,         #  following protein, pocket, ligand and native_ligand arguments are gates to convert specific molecule type from PDB complex
+                     pocket=False,
+                     ligand=False,
+                     native_ligand=False):
         '''
-        U will need MGL Tools Package and Obabel
+        This method converts the specified molecule from .pdb into .pdbqt format
+        You will need:
+            * MGL Tools PackageObabel
+            * OpenBabel
         '''
+
+        self.converting_dictionary[self.molecule]['pdb_to_pdbqt']={}
+
         if protein==False and pocket==False and ligand == False and native_ligand == False:
-            print('There is nothing to do. Precise which molecule type u want to convert (protein/pocket/ligand/native ligand)')
-            return None
+
+            self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['error'] = 'Argument Error. There is nothing to do. Please specify in settings.py which molecule type you want to convert (protein/pocket/ligand/native_ligand)'
 
         if protein == True:
-
-            print('<PROTEIN> MGL PDB TO PDBQT:')
 
             pdb_protein_file = self.datadir + '/protein/pdb/' + self.molecule + '_protein.pdb'
             pdbqt_protein_file = self.datadir + '/protein/pdbqt/' + self.molecule + '_protein.pdbqt'
 
+            self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein'] = {}
+
             if (not os.path.exists(pdbqt_protein_file) or os.path.getsize(pdbqt_protein_file) == 0) and os.path.exists(pdb_protein_file):
-                command = [settings.mgltools_dir+'/bin/pythonsh',settings.mgltools_dir + '/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py','-r',pdb_protein_file,'-o',pdbqt_protein_file,'-A','checkhydrogens']
+
+                command = [settings.mgltools_dir + '/bin/pythonsh', settings.mgltools_dir + '/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py',
+                           '-r',pdb_protein_file,
+                           '-o',pdbqt_protein_file,
+                           '-A','checkhydrogens']
+
                 pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
-                print('error: '+str(len(pdbqt_result.stderr))+' | '+pdbqt_result.stderr)
+
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein']['mgl_tools'] = {'output':pdbqt_result.stdout}
 
                 if int(len(pdbqt_result.stderr)) > 0:
 
-                    print('<PROTEIN> OPENBABEL PDB TO PDBQT:')
-                    command = [settings.obabel_path,pdb_protein_file,'-O',pdbqt_protein_file]
+                    self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein']['mgl_tools'] = {
+                        'error': pdbqt_result.stderr}
+
+                    command = [settings.obabel_path,
+                               pdb_protein_file,
+                               '-O',pdbqt_protein_file]
+
                     pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
-                    print('error: ' + str(len(pdbqt_result.stderr)) + ' | ' + pdbqt_result.stderr)
+
+                    self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein']['openbabel'] = {
+                        'output': pdbqt_result.stdout}
+
+                    if int(len(pdbqt_result.stderr)) > 0:
+                        self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein']['openbabel'] = {
+                            'error': pdbqt_result.err}
 
             else:
-
-                pdbqt_convert_error = self.molecule+', There is no PDB Protein file to convert or file already exists.'
-                print(pdbqt_convert_error)
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['protein']['error'] = 'File error. There is some problem with pdbqt file or pdb file has already exist.'
 
         if pocket == True:
-
-            print('<POCKET> MGL PDB TO PDBQT:')
 
             pdb_pocket_file = self.datadir + '/pocket/pdb/' + self.molecule + '_pocket.pdb'
             pdbqt_pocket_file = self.datadir + '/pocket/pdbqt/' + self.molecule + '_pocket.pdbqt'
 
+            self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket'] = {}
+
             if (not os.path.exists(pdbqt_pocket_file) or os.path.getsize(pdbqt_pocket_file) == 0) and os.path.exists(pdb_pocket_file):
-                command = [settings.mgltools_dir + '/bin/pythonsh',settings.mgltools_dir + '/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py','-r', pdb_pocket_file, '-o', pdbqt_pocket_file, '-A', 'checkhydrogens']
+
+                command = [settings.mgltools_dir + '/bin/pythonsh',settings.mgltools_dir + '/MGLToolsPckgs/AutoDockTools/Utilities24/prepare_receptor4.py',
+                           '-r', pdb_pocket_file,
+                           '-o', pdbqt_pocket_file,
+                           '-A', 'checkhydrogens']
+
                 pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
-                print('error: '+str(len(pdbqt_result.stderr))+' | '+pdbqt_result.stderr)
+
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket']['mgl_tools'] = {'output':pdbqt_result.stdout}
 
                 if int(len(pdbqt_result.stderr)) > 0:
 
-                        print('<POCKET> OPENBABEL PDB TO PDBQT:')
+                    self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket']['mgl_tools'] = {
+                        'error': pdbqt_result.stderr}
 
-                        command = [settings.obabel_path,pdb_pocket_file,'-O',pdbqt_pocket_file]
-                        pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
+                    command = [settings.obabel_path,pdb_pocket_file,
+                               '-O',pdbqt_pocket_file]
 
-                        print('error: ' + str(len(pdbqt_result.stderr)) + ' | ' + pdbqt_result.stderr)
+                    pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
+
+                    self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket']['openbabel'] = {
+                        'output': pdbqt_result.stdout}
+
+                    if int(len(pdbqt_result.stderr)) > 0:
+
+                        self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket']['openbabel'] = {
+                            'error': pdbqt_result.err}
             else:
-
-                pdbqt_convert_error = self.molecule+', There is no PDB Pocket file to convert or file already exists..'
-                print(pdbqt_convert_error)
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['pocket']['error'] = 'File error. There is some problem with pdbqt file or pdb file has already exist.'
 
         if ligand == True:
 
-            print('<LIGAND> OPENBABEL PDB TO PDBQT: ')
-
             pdb_ligand_file = self.datadir + '/ligand/pdb/' + self.molecule + '_ligand.pdb'
             pdbqt_ligand_file = self.datadir + '/ligand/pdbqt/' + self.molecule + '_ligand.pdbqt'
+
+            self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['ligand'] = {}
 
             if (not os.path.exists(pdbqt_ligand_file) or os.path.getsize(pdbqt_ligand_file) == 0) and os.path.exists(pdb_ligand_file):
 
                 command = [settings.obabel_path,pdb_ligand_file,'-O',pdbqt_ligand_file]
                 pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
-                print('error: '+str(len(pdbqt_result.stderr))+' | '+pdbqt_result.stderr)
+
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['ligand']['openbabel'] = {'output':pdbqt_result.stdout}
 
             else:
 
-                pdbqt_convert_error = self.molecule+', There is no PDB Ligand file to convert or file already exists..'
-                print(pdbqt_convert_error)
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['ligand']['error'] = 'File error. There is some problem with pdbqt file or pdb file has already exist.'
 
         if native_ligand == True:
 
-            print('<NATIVE LIGAND> PDB TO PDBQT:')
-
             pdb_native_ligand_file = self.datadir + '/native_ligand/pdb/' + self.molecule + '_ligand.pdb'
             pdbqt_native_ligand_file = self.datadir + '/native_ligand/pdbqt/' + self.molecule + '_ligand.pdbqt'
+
+            self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['native_ligand'] = {}
+
 
             if (not os.path.exists(pdbqt_native_ligand_file) or os.path.getsize(pdbqt_native_ligand_file) == 0) and os.path.exists(pdb_native_ligand_file):
 
                 command = [settings.obabel_path, pdb_native_ligand_file, '-O', pdbqt_native_ligand_file]
                 pdbqt_result = subprocess.run(command, shell=False, capture_output=True, text=True, close_fds=True)
-                print('error: '+str(len(pdbqt_result.stderr))+' | '+pdbqt_result.stderr)
+
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['native_ligand']['openbabel'] = {'output':pdbqt_result.stdout}
 
             else:
-                pdbqt_convert_error = self.molecule+', There is no PDB Native Ligand file to convert or file already exists..'
-                print(pdbqt_convert_error)
+                self.converting_dictionary[self.molecule]['pdb_to_pdbqt']['native_ligand']['error'] = 'File error. There is some problem with pdbqt file or pdb file has already exist.'
 
-    def pdb_to_seq(self,protein=True,pocket=False):
+    def pdb_to_seq(self,
+                   protein=True,         #  following protein, pocket arguments are gates to convert specific molecule type from PDB complex
+                   pocket=False):
+
+        self.converting_dictionary[self.molecule]['pdb_to_seq'] = {}
 
         if protein == False and pocket == False:
-            print('There is no molecule to convert. PLease specify what type of molecules you want to convert (protein/pocket).')
-            return None
+            self.converting_dictionary[self.molecule]['pdb_to_seq']['error'] = 'Argument Error. There is nothing to do. Please specify in settings.py which molecule type you want to convert (protein/pocket)'
 
         if protein==True:
+
             protein_pdb_file = self.datadir + '/protein/pdb/' + self.molecule + '_protein.pdb'
             protein_fasta_file = self.datadir + '/protein/fasta/' + self.molecule + '_protein.fasta'
+
+            self.converting_dictionary[self.molecule]['pdb_to_seq']['protein'] = {}
+
             if os.path.exists(protein_pdb_file) and not os.path.exists(protein_fasta_file):
+
+                self.converting_dictionary[self.molecule]['pdb_to_seq']['protein']['pdb2fasta'] = {}
+
                 result = subprocess.run([settings.pdb2fasta_path+' '+protein_pdb_file+' > '+protein_fasta_file], shell=False, capture_output=True, text=True, close_fds=True)
-                print(result.stderr)
+
+                self.converting_dictionary[self.molecule]['pdb_to_seq']['protein']['pdb2fasta']['output'] = result.stdout
+                self.converting_dictionary[self.molecule]['pdb_to_seq']['protein']['pdb2fasta']['error'] = result.stderr
+            else:
+                self.converting_dictionary[self.molecule]['pdb_to_seq']['protein']['error'] = 'File Error.'
 
         if pocket==True:
             pocket_pdb_file = self.datadir+'/pocket/pdb/'+self.molecule+'_pocket.pdb'
@@ -247,7 +307,11 @@ class Converter:
             if os.path.exists(pocket_pdb_file) and not os.path.exists(pocket_fasta_file):
                 result = subprocess.run([settings.pdb2fasta_path+' ' + pocket_pdb_file + ' > ' + pocket_fasta_file], shell=False,capture_output=True, text=True, close_fds=True)
                 print(result.stderr)
-    def pdb_to_mol(self,protein=False,pocket=False,ligand=False,native_ligand=False):
+    def pdb_to_mol(self,
+                   protein=False,         #  following protein, pocket, ligand and native_ligand arguments are gates to convert specific molecule type from PDB complex
+                   pocket=False,
+                   ligand=False,
+                   native_ligand=False):
 
         '''Convert pdb files to mol2 format'''
 
@@ -312,7 +376,11 @@ class Converter:
 
                 mol2_convert_error = self.molecule + ', There is no PDB file to convert or file already exists..'
                 print(mol2_convert_error)
-    def mol_to_pdb(self,protein=False,pocket=False,ligand=False,native_ligand=False):
+    def mol_to_pdb(self,
+                   protein=False,         #  following protein, pocket, ligand and native_ligand arguments are gates to convert specific molecule type from PDB complex
+                   pocket=False,
+                   ligand=False,
+                   native_ligand=False):
         '''To be done if needed'''
 
         '''Convert mol2 files to pdb format'''
@@ -379,7 +447,11 @@ class Converter:
                 pdb_convert_error = self.molecule + ', There is no PDB file to convert or file already exists..'
                 print(pdb_convert_error)
 
-    def mol_to_sdf(self,protein=False,pocket=False,ligand=False,native_ligand=False):
+    def mol_to_sdf(self,
+                   protein=False,         #  following protein, pocket, ligand and native_ligand arguments are gates to convert specific molecule type from PDB complex
+                   pocket=False,
+                   ligand=False,
+                   native_ligand=False):
         '''To be done if needed'''
 
         '''Convert sdf files to mol format'''
@@ -445,6 +517,7 @@ class Converter:
 
                 convert_error = self.molecule + ', There is no PDB file to convert or file already exists..'
                 print(convert_error)
+
 class Documents:
     def __init__(self,datadir):
         self.datadir = datadir
