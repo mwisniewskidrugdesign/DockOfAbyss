f1f924ec69e6d47f930d7f8e8275513a5d8192c4
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-02-12 17:43:46 +0100

    diffdock

--- utils/files.py
+++ utils/files.py
@@ -60,8 +60,9 @@ def diffdock_output_files_checker(output_dir='',csv_file=''):
     if os.path.exists(output_dir):
         diffdock_files = [file for file in os.listdir(output_dir)
                           if os.path.isfile(os.path.join(output_dir, file)) and os.path.getsize(os.path.join(output_dir, file)) > 0]
-
-    directory_checker = len(diffdock_files) >= 50
+        directory_checker = len(diffdock_files) >= 50
+    else:
+        directory_checker = False
     csv_checker = os.path.exists(csv_file)
 
     return directory_checker, csv_checker
