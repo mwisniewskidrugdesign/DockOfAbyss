6dfa2bfe20f03e71424ad7df0f92e361b519244b
maciejwisniewski <m.wisniewski@datascience.edu.pl>
2024-02-15 14:54:23 +0100

    diffdock again

--- settings.py
+++ settings.py
@@ -65,8 +65,8 @@ def init():
 
     number_of_models = 50
     pipeline = 'lp_pdbbind'
-    steps = ['convert']  # list of steps to run in your pipeline in list format: generate_library, convert, docking, matrix
-    docking_programs = []  # list of docking programs to run in your pipeline
+    steps = ['docking']  # list of steps to run in your pipeline in list format: generate_library, convert, docking, matrix
+    docking_programs = ['diffdock']  # list of docking programs to run in your pipeline
     matrix_programs = []  # list of matrices to generate in your pipeline for specific programs in string format
 
     if 'convert' in steps:
